3b2183af298b3409e771b82dfaf259307563fcb4
JShelto <jshelto@sas.upenn.edu>
2013-09-03 13:25:33 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crunchmate::Application.config.secret_token = '4a7ee414b3ca298ca8d378bc816cb6d85400d48e51f78b5ab4b616e84b868c873ed258002e24aa909e88e5bcb01879b273acc7caee85ee8975160a631f390c4b'
